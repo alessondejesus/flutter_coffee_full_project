e781ab4ff52aa16bdb7b94a841bef82a95ac8e71
alessondejesus <asn9006@hotmail.com>
2021-10-22 09:54:05 -0300

    feat: customizando default buttons e text

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: () => GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(
+                Color.fromRGBO(50, 74, 89, 1),
+              ), //button color
+              foregroundColor: MaterialStateProperty.all<Color>(
+                Colors.white,
+              ), //text (and icon)
+            ),
+          ),
+          buttonTheme: ButtonThemeData(
+            buttonColor: Color.fromRGBO(50, 74, 89, 1),
+            textTheme: ButtonTextTheme.primary,
+          ),
+          textTheme: GoogleFonts.poppinsTextTheme(
+            Theme.of(context).textTheme,
+          ).apply(
+            bodyColor: Color.fromRGBO(24, 29, 45, 1),
+          ),
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              children: [
+                ElevatedButton(onPressed: () => {}, child: Text('Button')),
+                Text('Text Center'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
