c0dd7eeab8b3c42877ed150e1b1b62db46086873
alessondejesus <asn9006@hotmail.com>
2021-10-22 17:16:22 -0300

    feat: parte 1 tela de intro home

---
+++ lib/providers/intros/IntroHomeProvider.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_intro/flutter_intro.dart';
+
+class IntroHomeProvider extends ChangeNotifier {
+  late Intro _intro;
+
+  Intro get count => _intro;
+
+  void config() {
+    _intro = Intro(
+      stepCount: 4,
+      maskClosable: true,
+      onHighlightWidgetTap: (introStatus) {
+        print(introStatus);
+      },
+
+      /// use defaultTheme
+      widgetBuilder: StepWidgetBuilder.useDefaultTheme(
+        texts: [
+          'Hello, I\'m Flutter Intro.',
+          'I can help you quickly implement the Step By Step guide in the Flutter project.',
+          'My usage is also very simple, you can quickly learn and use it through example and api documentation.',
+          'In order to quickly implement the guidance, I also provide a set of out-of-the-box themes, I wish you all a happy use, goodbye!',
+        ],
+        buttonTextBuilder: (currPage, totalPage) {
+          return currPage < totalPage - 1 ? 'Next' : 'Finish';
+        },
+      ),
+    );
+
+    _intro.setStepConfig(
+      0,
+      borderRadius: BorderRadius.circular(64),
+    );
+
+    notifyListeners();
+  }
+
+  void start(BuildContext context) {
+    _intro.start(context);
+
+    notifyListeners();
+  }
+
+  bool stop() {
+    IntroStatus introStatus = _intro.getStatus();
+
+    if (introStatus.isOpen) {
+      _intro.dispose();
+      notifyListeners();
+
+      return false;
+    }
+    notifyListeners();
+
+    return true;
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_intro/flutter_intro.dart';
 
@@ -9,81 +7,13 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StartPage(),
-    );
-  }
-}
-
-class StartPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Intro'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              child: Text('Start with useDefaultTheme'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => MyHomePage(
-                      title: 'Flutter Intro',
-                      mode: Mode.defaultTheme,
-                    ),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            ElevatedButton(
-              child: Text('Start with useAdvancedTheme'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => MyHomePage(
-                      title: 'Flutter Intro',
-                      mode: Mode.advancedTheme,
-                    ),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            ElevatedButton(
-              child: Text('Start with customTheme'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => MyHomePage(
-                      title: 'Flutter Intro',
-                      mode: Mode.customTheme,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+      body: MyHomePage(),
     );
   }
 }
 
 enum Mode {
   defaultTheme,
-  customTheme,
-  advancedTheme,
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -92,174 +22,38 @@ class _MyHomePageState extends State<MyHomePage> {
   _MyHomePageState({
     Mode? mode,
   }) {
-    if (mode == Mode.defaultTheme) {
-      /// init Intro
-      intro = Intro(
-        stepCount: 4,
-        maskClosable: true,
-        onHighlightWidgetTap: (introStatus) {
-          print(introStatus);
-        },
-
-        /// use defaultTheme
-        widgetBuilder: StepWidgetBuilder.useDefaultTheme(
-          texts: [
-            'Hello, I\'m Flutter Intro.',
-            'I can help you quickly implement the Step By Step guide in the Flutter project.',
-            'My usage is also very simple, you can quickly learn and use it through example and api documentation.',
-            'In order to quickly implement the guidance, I also provide a set of out-of-the-box themes, I wish you all a happy use, goodbye!',
-          ],
-          buttonTextBuilder: (currPage, totalPage) {
-            return currPage < totalPage - 1 ? 'Next' : 'Finish';
-          },
-        ),
-      );
-      intro.setStepConfig(
-        0,
-        borderRadius: BorderRadius.circular(64),
-      );
-    }
-    if (mode == Mode.advancedTheme) {
-      /// init Intro
-      intro = Intro(
-        stepCount: 4,
-        maskClosable: false,
-        onHighlightWidgetTap: (introStatus) {
-          print(introStatus);
-        },
-
-        /// useAdvancedTheme
-        widgetBuilder: StepWidgetBuilder.useAdvancedTheme(
-          widgetBuilder: (params) {
-            return Container(
-              decoration: BoxDecoration(
-                color: Colors.red.withOpacity(.6),
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    '${params.currentStepIndex + 1}/${params.stepCount}',
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      ElevatedButton(
-                        onPressed: params.onPrev,
-                        child: Text('Prev'),
-                      ),
-                      ElevatedButton(
-                        onPressed: params.onNext,
-                        child: Text('Next'),
-                      ),
-                      ElevatedButton(
-                        onPressed: params.onFinish,
-                        child: Text('Finish'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
-      );
-      intro.setStepConfig(
-        0,
-        borderRadius: BorderRadius.circular(64),
-      );
-    }
-    if (mode == Mode.customTheme) {
-      /// init Intro
-      intro = Intro(
-        stepCount: 4,
-
-        maskClosable: true,
-
-        /// implement widgetBuilder function
-        widgetBuilder: customThemeWidgetBuilder,
-      );
-    }
-  }
-
-  Widget customThemeWidgetBuilder(StepWidgetParams stepWidgetParams) {
-    List<String> texts = [
-      'Hello, I\'m Flutter Intro.',
-      'I can help you quickly implement the Step By Step guide in the Flutter project.',
-      'My usage is also very simple, you can quickly learn and use it through example and api documentation.',
-      'In order to quickly implement the guidance, I also provide a set of out-of-the-box themes, I wish you all a happy use, goodbye!',
-    ];
-    return Padding(
-      padding: EdgeInsets.all(
-        32,
-      ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          Text(
-            '${texts[stepWidgetParams.currentStepIndex]}【${stepWidgetParams.currentStepIndex + 1} / ${stepWidgetParams.stepCount}】',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-            ),
-          ),
-          Row(
-            children: [
-              ElevatedButton(
-                onPressed: stepWidgetParams.onPrev,
-                child: Text(
-                  'Prev',
-                ),
-              ),
-              SizedBox(
-                width: 16,
-              ),
-              ElevatedButton(
-                onPressed: stepWidgetParams.onNext,
-                child: Text(
-                  'Next',
-                ),
-              ),
-              SizedBox(
-                width: 16,
-              ),
-              ElevatedButton(
-                onPressed: stepWidgetParams.onFinish,
-                child: Text(
-                  'Finish',
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-      Duration(
-        milliseconds: 500,
-      ),
-      () {
-        /// start the intro
-        intro.start(context);
+    intro = Intro(
+      stepCount: 4,
+      maskClosable: true,
+      onHighlightWidgetTap: (introStatus) {
+        print(introStatus);
       },
+
+      /// use defaultTheme
+      widgetBuilder: StepWidgetBuilder.useDefaultTheme(
+        texts: [
+          'Hello, I\'m Flutter Intro.',
+          'I can help you quickly implement the Step By Step guide in the Flutter project.',
+          'My usage is also very simple, you can quickly learn and use it through example and api documentation.',
+          'In order to quickly implement the guidance, I also provide a set of out-of-the-box themes, I wish you all a happy use, goodbye!',
+        ],
+        buttonTextBuilder: (currPage, totalPage) {
+          return currPage < totalPage - 1 ? 'Next' : 'Finish';
+        },
+      ),
+    );
+    intro.setStepConfig(
+      0,
+      borderRadius: BorderRadius.circular(64),
     );
   }
 
+  //todos os elementos que serão destacados
   Widget build(BuildContext context) {
     return WillPopScope(
       child: Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Text('Flutter intro'),
         ),
         body: SingleChildScrollView(
           child: Container(
@@ -282,9 +76,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   /// 3rd guide
                   key: intro.keys[2],
                   fallbackHeight: 100,
-                ),
-                SizedBox(
-                  height: 16,
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -315,10 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       onWillPop: () async {
-        // sometimes you need get current status
         IntroStatus introStatus = intro.getStatus();
         if (introStatus.isOpen) {
-          // destroy guide page when tap back key
           intro.dispose();
           return false;
         }
@@ -331,16 +120,10 @@ class _MyHomePageState extends State<MyHomePage> {
 class MyHomePage extends StatefulWidget {
   MyHomePage({
     Key? key,
-    required this.title,
-    required this.mode,
   }) : super(key: key);
-
-  final String title;
-
-  final Mode mode;
 
   @override
   _MyHomePageState createState() => _MyHomePageState(
-        mode: mode,
+        mode: Mode.defaultTheme,
       );
 }
